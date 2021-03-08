1c60de725c2e7ef0e9affe989412f674b0f102ff
Shadryn <jsmycky@gmail.com>
2021-03-08 11:40:03 -0500

    buttons#139 - WIP Button loading indicator

---
+++ packages/react-magma-dom/src/components/StyledButton/StyledButton.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyledButton } from '.';
+
+export default {
+  component: StyledButton,
+  title: 'StyledButton',
+};
+
+export const Default = () => {
+  return < StyledButton isLoading isInverse> text </ StyledButton >;
+};
