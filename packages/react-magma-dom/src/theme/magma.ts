6446b84c6fbf27a940856377b7804ebcb2fca344
Jamie <jsmycky@gmail.com>
2021-04-28 10:28:56 -0400

    Drawer WIP

--- packages/react-magma-dom/src/theme/magma.ts
+++ packages/react-magma-dom/src/theme/magma.ts
@@ -146,8 +146,39 @@ export interface Modal {
   };
 }
 
+interface Drawer {
+  default: {
+    maxWidth: string;
+    borderRadius: string;
+    margin: string;
+    top: string;
+    bottom: string;
+    left: string;
+    right: string;
+    position: string;
+  };
+  top: {
+    bottom: string;
+  };
+  bottom: {
+    top: string;
+  };
+  left: {
+    right: string;
+  };
+  right: {
+    left: string;
+  };
+}
+
 interface Combobox {
   menu: {
+    maxHeight: string;
+  };
+}
+
+interface Dropdown {
+  content: {
     maxHeight: string;
   };
 }
@@ -193,6 +224,7 @@ export interface ThemeInterface {
   colors: Colors;
   combobox: Combobox;
   direction: string;
+  drawer: Drawer;
   dropdown: Dropdown;
   headingFont: string;
   iconSizes: IconSizes;
@@ -526,6 +558,31 @@ export const magma = {
       small: '300px',
       medium: '600px',
       large: '900px',
+    },
+  },
+
+  drawer: {
+    default: {
+      maxWidth: '100%',
+      margin: '0',
+      borderRadius: '0',
+      top: '0',
+      right: '0',
+      bottom: '0',
+      left: '0',
+      position: 'absolute',
+    },
+    right: {
+      left: 'auto',
+    },
+    left: {
+      right: 'auto',
+    },
+    top: {
+      bottom: 'auto',
+    },
+    bottom: {
+      top: 'auto',
     },
   },
 
